29e6adafe5cae78de811e01422156e303bb67981
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-06 15:00:37 +0300

    Initial Commit

---
+++ Todift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todift
+//
+//  Created by Omer Cagri Sayir on 6.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todift/TodiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodiftApp.swift
+//  Todift
+//
+//  Created by Omer Cagri Sayir on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
