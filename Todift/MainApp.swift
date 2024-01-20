2b67b439d69ab4331218a760128da850a73dd472
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-20 12:09:15 +0300

    Firebase Init

--- Todift/MainApp.swift
+++ Todift/MainApp.swift
@@ -5,10 +5,22 @@
 //  Created by Omer Cagri Sayir on 6.01.2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct MainApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             TodosListView()
