ea23e2714416e72ee600d5bbe92c3c3d7a725275
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-22 15:31:17 +0300

    added googles id

--- Todift/Components/AdBannerView.swift
+++ Todift/Components/AdBannerView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 
 // UIViewRepresentable wrapper for AdMob banner view
 struct AdBannerView: UIViewRepresentable {
+    // Google's for try: ca-app-pub-3940256099942544/2934735716
     let adUnitID: String
-
+ 
     func makeUIView(context: Context) -> GADBannerView {
         let bannerView = GADBannerView(adSize: GADAdSizeFromCGSize(CGSize(width: 320, height: 50))) // Set your desired banner ad size
         bannerView.adUnitID = adUnitID
