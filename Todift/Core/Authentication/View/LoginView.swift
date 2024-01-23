4b07b73bf7a420b776746d377cb2f38a5f732638
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-23 15:11:12 +0300

    Login page design

--- Todift/Core/Authentication/View/LoginView.swift
+++ Todift/Core/Authentication/View/LoginView.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct LoginView: View {
+//    @EnvironmentObject var viewModel: AuthViewModel
+
     @State var email = ""
     @State var password = ""
-    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // image
+                Image("splash-screen-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 220)
+                    .padding(.vertical, 32)
+
+                // form fields
+                VStack(spacing: 24) {
+                    InputView(text: $email, title: "Email Address", placeholder: "name@example.com", systemName: "mail")
+
+                    InputView(text: $password, title: "Password", placeholder: "Enter your password", isSecureField: true, systemName: "lock")
+                }
+                .textInputAutocapitalization(.never)
+                .padding(.horizontal)
+                .padding(.top, 12)
+
+                // sign in button
+                Button {
+//                    Task {
+//                        try await viewModel.login(withEmail: email, password: password)
+//                    }
+                } label: {
+                    HStack {
+                        Text("Sign In")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }.foregroundStyle(.white)
+                        .frame(width: 300, height: 48)
+                }
+                .background(Color(.systemBlue))
+//                .disabled(!formIsValid)
+//                .opacity(formIsValid ? 1.0 : 0.5)
+                .clipShape(.buttonBorder)
+                .padding(.top, 24)
+
+                Spacer()
+                // sign up button
+                NavigationLink {
+//                    RegisterView()
+//                        .navigationBarBackButtonHidden()
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                    }.font(.system(size: 14))
+                }
+            }
+        }
     }
 }
 
